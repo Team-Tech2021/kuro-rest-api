a9031bf2d70b248faeb788f95d95f5801d0c0e9e
ManarAbdelkarim <manarbdelkarim434@gmail.com>
2021-07-15 04:25:27 +0300

    adding tests and requierments

--- CodeEditor/tests.py
+++ CodeEditor/tests.py
@@ -7,18 +7,21 @@ import json
 
 class FunctionsTest(TestCase):
     def test_tokens(self):
+
+            '''try it with a real user and the server running'''
+
+
             self.url = "http://127.0.0.1:8000/api/token/"
+            # self.payload = json.dumps({
+            # "username": "kuro",
+            # "password": "koko@123"
+            # })
+            # self.headers = {
+            # 'Content-Type': 'application/json'
+            # }
 
-            self.payload = json.dumps({
-            "username": "kuro",
-            "password": "koko@123"
-            })
-            self.headers = {
-            'Content-Type': 'application/json'
-            }
+            # self.response = requests.request("POST", self.url, headers=self.headers, data=self.payload)
 
-            self.response = requests.request("POST", self.url, headers=self.headers, data=self.payload)
+            # first = self.response.text.split(':')
+            # assert first[0] == '{"refresh"'
 
-            first = self.response.text.split(':')
-            assert first[0] == '{"refresh"'
-   
